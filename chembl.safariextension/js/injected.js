1728c3a5314f49443eca8c8ad5c7187379b27802
Matt Swain <m.swain@me.com>
2014-03-20 16:09:20 +0000

    Fix CSS to prevent clashes with other extensions

--- chembl.safariextension/js/injected.js
+++ chembl.safariextension/js/injected.js
@@ -25,30 +25,29 @@ function showLoading(id) {
         $('<link rel="stylesheet" href="'+safari.extension.baseURI+'css/chembl.css">').appendTo('head');
 	}
     var dialog = $('<div class="chembl-safari-extension" />').attr('id', id),
-        title = $('<div class="cssse-title">ChEMBL</div>')
-        spinner = $('<div class="cssse-spinner" />'),
-        close = $('<button type="button" class="cssse-close">×</button>')
+        title = $('<div class="chsse-title">ChEMBL</div>')
+        spinner = $('<div class="chsse-spinner" />'),
+        close = $('<button type="button" class="chsse-close">×</button>')
     close.click(function() { $(this).parent().fadeOut(); });
     title.css({backgroundImage: 'url('+safari.extension.baseURI+'img/icon-small.png)'});
     dialog.append(close).append(title).append(spinner);
     dialog.css({top: locY, left: locX, display: 'none'});
-    dialog.drags({handle: '.cssse-title'})
+    dialog.drags({handle: '.chsse-title'})
     dialog.appendTo('body').fadeIn();
 }
 
 function displayResult(msg) {
     var dialog = $('#'+msg.id);
-    $('div.cssse-spinner', dialog).fadeOut(function() {
-    	console.log(msg.results);
+    $('div.chsse-spinner', dialog).fadeOut(function() {
         if (msg.results.aaData.length < 1) {
-            $('<div class="cssse-noresults">No Results Found</div>').appendTo(dialog);
+            $('<div class="chsse-noresults">No Results Found</div>').appendTo(dialog);
         } else {
             if (msg.results.aaData.length > 1) {
-		        $('<button type="button" class="cssse-prev cssse-arrow">⟨</button>').click(function() {
-		            $('.cssse-slider', $(this).parent()).scrollTo({top:0, left:'-=175'}, 400);
+		        $('<button type="button" class="chsse-prev chsse-arrow">⟨</button>').click(function() {
+		            $('.chsse-slider', $(this).parent()).scrollTo({top:0, left:'-=175'}, 400);
 		        }).appendTo(dialog);
 		    }
-            var slider = $('<div class="cssse-slider" />');
+            var slider = $('<div class="chsse-slider" />');
             $.each(msg.results.aaData, function(i, result) {
             	var title = result[1];
             	if (result[2] !== null) {
@@ -58,24 +57,24 @@ function displayResult(msg) {
         			}
             	}
                 var img = $('<img src="https://www.ebi.ac.uk/chembl/compound/displayimage/'+result[0]+'" title="'+title+'">'),
-                    save = $('<button type="button" title="Save MOL file for '+result[1]+'" class="cssse-save">Save</button>'),
-                    view = $('<button type="button" title="View record page for '+result[1]+'" class="cssse-view">View</button>'),
-                    cap = $('<div class="cssse-caption">CHEMBL<span class="cssse-cid">'+result[1].substring(6)+'</span></div>');
+                    save = $('<button type="button" title="Save MOL file for '+result[1]+'" class="chsse-save">Save</button>'),
+                    view = $('<button type="button" title="View record page for '+result[1]+'" class="chsse-view">View</button>'),
+                    cap = $('<div class="chsse-caption">CHEMBL<span class="chsse-cid">'+result[1].substring(6)+'</span></div>');
                 save.click(function() {
                     location.href = 'https://www.ebi.ac.uk/chembl/download_helper/getmol/'+result[0];
                 });
                 view.click(function() {
                     safari.self.tab.dispatchMessage('viewCompound', result[1]);
                 });
-			    $('<div class="cssse-mol" />').append(img).append(save).append(view).append(cap).appendTo(slider);
+			    $('<div class="chsse-mol" />').append(img).append(save).append(view).append(cap).appendTo(slider);
 		    });
 		    slider.hide().appendTo(dialog);
 		    dialog.animate({height: slider.height()+36}, 300, function () {
 		        slider.fadeIn(300);
             });
 		    if (msg.results.aaData.length > 1) {
-		        $('<button type="button" class="cssse-next cssse-arrow">⟩</button>').click(function() {
-		            $('.cssse-slider', $(this).parent()).scrollTo({top:0, left:'+=175'}, 400);
+		        $('<button type="button" class="chsse-next chsse-arrow">⟩</button>').click(function() {
+		            $('.chsse-slider', $(this).parent()).scrollTo({top:0, left:'+=175'}, 400);
 		        }).appendTo(dialog);
 		    }
 
@@ -89,23 +88,23 @@ function displayResult(msg) {
         opt = $.extend({cursor:'move'}, opt);
         var $el = this.find(opt.handle);
         return $el.css('cursor', opt.cursor).on('mousedown', function(e) {
-            var $drag = $(this).addClass('cssse-active-handle').parent().addClass('cssse-draggable');
+            var $drag = $(this).addClass('chsse-active-handle').parent().addClass('chsse-draggable');
             locZ = Math.max(5001, locZ+1);
             var drg_h = $drag.outerHeight(),
                 drg_w = $drag.outerWidth(),
                 pos_y = $drag.offset().top + drg_h - e.pageY,
                 pos_x = $drag.offset().left + drg_w - e.pageX;
             $drag.css('z-index', locZ).parents().on('mousemove', function(e) {
-                $('.cssse-draggable').offset({
+                $('.chsse-draggable').offset({
                     top:e.pageY + pos_y - drg_h,
                     left:e.pageX + pos_x - drg_w
                 }).on('mouseup', function() {
-                    $(this).removeClass('cssse-draggable');
+                    $(this).removeClass('chsse-draggable');
                 });
             });
             e.preventDefault(); // disable selection
         }).on('mouseup', function() {
-            $(this).removeClass('cssse-active-handle').parent().removeClass('cssse-draggable');
+            $(this).removeClass('chsse-active-handle').parent().removeClass('chsse-draggable');
         });
     }
 })(jQuery);
